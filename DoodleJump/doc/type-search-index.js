b1e7218533299044ab1405d722321092d45608ff
philipelb <125591697+philipelb@users.noreply.github.com>
2023-02-16 19:57:28 +0100

    Delete DoodleJump directory

--- DoodleJump/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Doodle"},{"p":"<Unnamed>","l":"DoodleJumpMain"},{"p":"<Unnamed>","l":"Platform"},{"p":"<Unnamed>","l":"scripture"}]
